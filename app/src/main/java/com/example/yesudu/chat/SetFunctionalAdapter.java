f2447c9f8cb3a2a31b7c802e99687ef1a8bb3db4
Alexandr Burdelev <alexbur228@gmail.com>
2021-01-01 15:58:13 +0300

    any changes to design

--- app/src/main/java/com/example/yesudu/chat/SetFunctionalAdapter.java
+++ app/src/main/java/com/example/yesudu/chat/SetFunctionalAdapter.java
@@ -1,5 +1,5 @@
 package com.example.yesudu.chat;
-//TODO подходящее название
+
 public interface SetFunctionalAdapter {
     void setMessageLayout(ChatMessage chatMessage);
     String generateKey();
